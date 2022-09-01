08c0b2e937682e77b3f5adaaefcc9b4a793ec3ee
scare-destiny <zhenya.venger@gmail.com>
2022-09-01 18:32:25 +0300

    updated menu color

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -42,13 +42,13 @@ const Navbar = () => {
       <div className="westernsattel__navbar-menu">
         {toggleMenu ? (
           <RiCloseLine
-            color="#fff"
+            color="#031b34"
             size={27}
             onClick={() => setToggleMenu(false)}
           />
         ) : (
           <RiMenu3Line
-            color="#fff"
+            color="#031b34"
             size={27}
             onClick={() => setToggleMenu(true)}
           />
